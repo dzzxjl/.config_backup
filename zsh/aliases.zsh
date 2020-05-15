76808f77bb291c02a8112f5253d9f77b635d4629
dzzxjl <dzzxjl@126.com>
2020-05-15 10:47:18 +0800

    update zsh/alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,20 +1,77 @@
-alias rm='rm -i'
-alias av='source venv/bin/activate'
+# base alias
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias l="ls -l"
+alias la="ls -a"
+alias ll="ls -lah"
+alias md="mkdir -p"
 alias c='clear'
-alias cdiff='colordiff'
+alias mv="mv -i"
+alias rm='rm -i'
+alias slink="link -s"
+alias duh='du -h -d 1'
+alias n='nvidia-smi'
+
+# cli-tools alias
+# alias vim='nvim'
+# alias s='neofetch'
+alias nf='neofetch'
+alias ta='tmux a'
+alias t='tmux'
 alias cs='calcurse'
-alias dv='deactivate'
-alias gc='git config credential.helper store'
-alias ipy='ipython'
-alias l='ls -lah'
-alias lg='lazygit'
-alias ms='mailsync'
-alias mt='neomutt'
-alias r='echo $RANGER_LEVEL'
-alias pu='python3 -m pudb'
 alias ra='ranger'
+alias r='echo $RANGER_LEVEL'
+alias lg='lazygit'
+
+# git alias
+alias gs="git status"
+alias gsm="git summary"
+alias ga='git add'
+alias gd='git diff'
+alias gf='git fetch'
+alias grv='git remote -v'
+alias grb='git rebase'
+alias gbr='git branch'
+alias gpl="git pull"
+alias gps="git push"
+alias gco="git checkout"
+alias gl="git log"
+alias gc="git commit -m"
+alias gm="git merge"
+
+# my-linux-alias
 alias chromium-v='chromium --proxy-server="socks5://127.0.0.1:1080"'
 alias fs='flameshot full -p ~/Pictures/Screenshots'
+
+# other cw alias
+
+# alias vb='vi ~/.bashrc'
+# alias sb='. ~/.bashrc'
+# alias sc='screen'
+# alias nvfind='fuser -v /dev/nvidia*'
+# alias nving='watch -n 0.1 nvidia-smi'
+
+
+# alias gc='git config credential.helper store'
+# alias gs='git config credential.helper store'
+# alias bat='sudo tlp bat'
+# alias ac='sudo tlp ac'
+# alias gy='git-yolo'
+# alias nb='newsboat -r'
+# alias dv='deactivate'
+# alias ipy='ipython'
+# alias ms='mailsync'
+# alias mt='neomutt'
+# alias pu='python3 -m pudb'
+# alias sra='sudo -E ranger'
+# alias sudo='sudo -E'
+# alias nt="sh -c 'cd $(pwd); st' > /dev/null 2>&1 &"
+# alias lo='lsof -p $(fps) +w'
+# alias av='source venv/bin/activate'
+# alias cdiff='colordiff'
+# alias g='onefetch'
+
 # ra() {
 	#if [ -z "$RANGER_LEVEL" ]
 	#then
@@ -23,17 +80,3 @@ alias fs='flameshot full -p ~/Pictures/Screenshots'
 		#exit
 	#fi
 #}
-# alias s='neofetch'
-alias g='onefetch'
-alias sra='sudo -E ranger'
-alias sudo='sudo -E'
-# alias vim='nvim'
-alias gs='git config credential.helper store'
-alias bat='sudo tlp bat'
-alias ac='sudo tlp ac'
-alias gy='git-yolo'
-alias nb='newsboat -r'
-# alias nt="sh -c 'cd $(pwd); st' > /dev/null 2>&1 &"
-alias ta='tmux a'
-alias t='tmux'
-# alias lo='lsof -p $(fps) +w'
