d7c4d6fbcf498e47b7f1e60ffa90b62fec70fb1c
dzzxjl <dzzxjl@gmail.com>
2025-12-20 21:43:00 +0800

    [feat] update nvim lazy-lock.json and add invisible character display options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- 显示不可见字符
+vim.opt.list = true
+vim.opt.listchars = {
+  tab = "▸ ",
+  trail = "·",
+}
