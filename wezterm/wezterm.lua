3e4e99bcd705365f49f473e6c809cd80174d5d21
dzzxjl <dzzxjl@gmail.com>
2025-12-23 22:35:10 +0800

    [feat] update .tmux.conf

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,48 +7,49 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
 config = {
-    font_size = 16,
-    color_scheme = 'AdventureTime',
-    -- color_scheme = 'Batman',
-    window_background_opacity = .9,
-    window_decorations = "RESIZE"
+	font_size = 16,
+	color_scheme = "AdventureTime",
+	-- color_scheme = 'Batman',
+	window_background_opacity = 0.9,
+	window_decorations = "RESIZE",
 }
 
-config.font =
-  wezterm.font('JetBrains Mono', { weight = 'Bold', italic = true })
+config.font = wezterm.font("JetBrains Mono", { weight = "Bold", italic = true })
 
 -- https://github.com/wez/wezterm/issues/253
 config.keys = {
-    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
-    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
-    -- Make Option-Right equivalent to Alt-f; forward-word
-    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+	-- Make Option-Right equivalent to Alt-f; forward-word
+	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
 }
 
 -- https://github.com/wez/wezterm/discussions/3541
 local act = wezterm.action
 config.mouse_bindings = {
-    {
-        event = { Down = { streak = 1, button = "Right" } },
-        mods = "NONE",
-        action = wezterm.action_callback(function(window, pane)
-            local has_selection = window:get_selection_text_for_pane(pane) ~= ""
-            if has_selection then
-                window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
-                window:perform_action(act.ClearSelection, pane)
-            else
-                window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
-            end
-        end),
-    },
+	{
+		event = { Down = { streak = 1, button = "Right" } },
+		mods = "NONE",
+		action = wezterm.action_callback(function(window, pane)
+			local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+			if has_selection then
+				window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+				window:perform_action(act.ClearSelection, pane)
+			else
+				window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
+			end
+		end),
+	},
 }
+
+config.window_close_confirmation = "NeverPrompt"
 
 -- and finally, return the configuration to wezterm
 return config
